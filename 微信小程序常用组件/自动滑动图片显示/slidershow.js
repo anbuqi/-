3169d935b815a7b60b9c981f7d4d9cf469502556
jeson <1785631570@qq.com>
2019-06-06 00:23:28 +0800

    添加更多小程序组件。

---
+++ 微信小程序常用组件/自动滑动图片显示/slidershow.js
@@ -0,0 +1,26 @@
+// components/slidershow.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imgUrls: ['https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
